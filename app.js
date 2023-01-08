caf2cede1b78bbe8c78eddc8064de96502b30592
Marius Hasan <marzusness@gmail.com>
2023-01-08 21:29:30 +0200

    testing

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
+const testingRouter = require('./controllers/testing')
 
 logger.info('connecting to', config.MONGODB_URI)
 
@@ -21,16 +22,14 @@ mongoose.connect(config.MONGODB_URI)
         logger.error('error connection to MongoDB:', error.message)
     })
 
-app.use(express.static('build'))
 app.use(cors())
+app.use(express.static('build'))
 app.use(express.json())
 app.use(middleware.tokenExtractor)
 app.use('/api/login', loginRouter)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 if (process.env.NODE_ENV === 'test') {
-    // eslint-disable-next-line global-require
-    const testingRouter = require('./controllers/testing')
     app.use('/api/testing', testingRouter)
 }
 app.use(middleware.errorHandler)
--- frontend/src/services/login.js
+++ frontend/src/services/login.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = 'http://localhost:3001/api/login'
+const baseUrl = 'http://localhost:3000/api/login'
 
 const login = async (credentials) => {
     const response = await axios.post(baseUrl, credentials)
