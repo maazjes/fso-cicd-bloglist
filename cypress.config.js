3456e1854c69d0ac30c72533d25c0353dab8c6fb
Marius Hasan <marzusness@gmail.com>
2023-01-08 23:50:35 +0200

    added projectid to cypress

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+    projectId: 's25vj5',
     e2e: {
         setupNodeEvents() {
             // implement node event listeners here
